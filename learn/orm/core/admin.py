89947a73eade38b6a46b0a26d51326c2a8c82e31
EricMuturiMwangi <muturiwamwanginanyawira@gmail.com>
2024-10-23 20:57:25 +0300

    create models

--- learn/orm/core/admin.py
+++ learn/orm/core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from core.models import Restaurant, Sale, Rating
 
-# Register your models here.
+
+admin.site.register(Restaurant)
+admin.site.register(Rating)
+admin.site.register(Sale)
