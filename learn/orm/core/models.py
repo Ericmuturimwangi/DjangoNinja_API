a7f6b1b8abc663c6ae7d128220d2fa9103736363
EricMuturiMwangi <muturiwamwanginanyawira@gmail.com>
2024-10-23 20:46:16 +0300

    create models

--- learn/orm/core/models.py
+++ learn/orm/core/models.py
@@ -1,13 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.core.validators import MinValueValidator, MaxValueValidator
-from django.core.exceptions import ValidationError
 
 
-def validate_restaurant_name_begins_with_a(value):
-    if not value.startswith('a'):
-        raise ValidationError('Restaurant name must begin with "a"')
-    
+
 
 class Restaurant(models.Model):
     class TypeChoices(models.TextChoices):
@@ -20,19 +15,29 @@ class Restaurant(models.Model):
         OTHER = 'OT', 'Other'
 
 
-    name = models.CharField(max_length=100, validators=[validate_restaurant_name_begins_with_a])
+    name = models.CharField(max_length=100)
     website = models.URLField(default='')
     date_opened = models.DateField()
-    latitude = models.FloatField(validators=[MinValueValidator(-90), MaxValueValidator(90)])
-    longitude = models.FloatField(validators=[MinValueValidator(-180), MaxValueValidator(180)])
-    restaurant_type = models.CharField(max_length=2, choices=TypeChoices.choices)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    restaurant_type = models.CharField(max_length=2, 
+                                       choices=TypeChoices.choices, default='', null=True )
 
+# thunderstring method
     def __str__(self):
         return self.name
     
+class Rating(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    rating = models.PositiveSmallIntegerField()
+    
+    def __str__(self):
+        return f"Rating: {self.rating}"
+    
 
-    def save(self, *args, **kwargs):
-        print(self._state.adding)
-        super().save(*args, **kwargs)
-
- 
+class Sale(models.Model):
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.SET_NULL, null=True)
+    income = models.DecimalField(max_digits=8, decimal_places=2)
+    datetime = models.DateTimeField()
+    
