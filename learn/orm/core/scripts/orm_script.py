5542ad30b6a163fa17e0104bd0a3740126f68ec9
EricMuturiMwangi <muturiwamwanginanyawira@gmail.com>
2024-10-24 19:52:12 +0300

    creating queries by using restaurant.objects.first()

---
+++ learn/orm/core/scripts/orm_script.py
@@ -0,0 +1,11 @@
+from core.models import Restaurant
+from django.utils import timezone
+from django.db import connection
+
+def run():
+
+    restaurant = Restaurant.objects.first()
+    print(restaurant)
+
+    print (connection.queries)
+
