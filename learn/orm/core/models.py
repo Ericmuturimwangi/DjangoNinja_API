fcb5b034008cc91be0b570a3c02a175f4f462f8b
EricMuturiMwangi <muturiwamwanginanyawira@gmail.com>
2024-10-27 08:52:32 +0300

    adding validators

--- learn/orm/core/models.py
+++ learn/orm/core/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 
@@ -28,10 +29,11 @@ class Restaurant(models.Model):
         return self.name
     
 class Rating(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
-    rating = models.PositiveSmallIntegerField()
-    
+    rating = models.PositiveSmallIntegerField(
+        validators = [MinValueValidator(1), MaxValueValidator(5)]
+    )
     def __str__(self):
         return f"Rating: {self.rating}"
     
--- learn/orm/core/scripts/orm_script.py
+++ learn/orm/core/scripts/orm_script.py
@@ -2,10 +2,15 @@ from django.contrib.auth.models import User
 from core.models import Restaurant, Rating
 from django.utils import timezone
 from django.db import connection
+from pprint import pprint
 
 def run():
     restaurant = Restaurant.objects.first()
     user = User.objects.first()
 
-    Rating.objects.create(user=user, restaurant=restaurant, rating = 3)
 
+    rating = Rating(user=user, restaurant=restaurant, rating=9)
+
+    rating.full_clean()
+    rating.save()
+    
